30c013223249aed9a3416e77bbcc37f157090573
Robert Shelton <robert.b.shelton.42@gmail.com>
2026-02-03 14:06:11 -0500

    add version, drop stop words in phrase

--- sql_redis/__init__.py
+++ sql_redis/__init__.py
@@ -1,5 +1,6 @@
 """SQL to Redis command translation utility."""
 
 from sql_redis.translator import TranslatedQuery, Translator
+from sql_redis.version import __version__
 
-__all__ = ["Translator", "TranslatedQuery"]
+__all__ = ["Translator", "TranslatedQuery", "__version__"]
